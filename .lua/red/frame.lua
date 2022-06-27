f4e891606b1b06abbc6ddf899697e29b1f42b6fb
w13b3 <w13b3>
2022-06-27 19:34:21 +0200

    :orange_circle:
    what happens when the page is not found

--- .lua/red/frame.lua
+++ .lua/red/frame.lua
@@ -1,20 +1,32 @@
 local Frame = {}
 Frame.__index = Frame
 
+
 function Frame:Page(path, method, func)
     method = tostring(method):upper()
-    self.paths[path] = method
+    self.paths[path] = self.paths[path] or {}
+    table.insert(self.paths[path], method)
     self.methods[method] = self.methods[method] or {}
     self.methods[method][path] = func
 end
+
 
 function Frame:RoutePath(path, method)
     method = method and tostring(method):upper() or GetMethod()
 
     local pages = self.methods[method]
+    if not pages then
+        return false, 405
+    end
+
     local func = pages[path]
+    if not func then
+        return false, 404
+    end
+
     return func()
-end
+    end
+
 
 function Frame.New(frameName)
     local newInstance = {
@@ -25,6 +37,7 @@ function Frame.New(frameName)
     return setmetatable(newInstance, Frame)
 end
 
+
 local MetaFrame = {
     __call = function(self, frameName)
         return Frame.New(frameName)
@@ -32,6 +45,9 @@ local MetaFrame = {
 }
 
 setmetatable(Frame, MetaFrame)
+
+
+-- local testing
 
 local frame = Frame("new frame")
 
@@ -41,7 +57,11 @@ end
 
 frame:Page("test", "get", sheet)
 
-local result = frame:RoutePath("test", "get")
-print(result)
+print("found: ", frame:RoutePath("test", "get"))
+print("not found: ", frame:RoutePath("test1", "get"))
+print("no method like this", frame:RoutePath("test", "post"))
+
+
+
 
 return Frame
