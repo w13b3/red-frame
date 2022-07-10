42f46913583446310af220fd9bac2ebb8929fc18
w13b3 <w13b3>
2022-07-10 21:16:06 +0200

    :mage_man:
    Updated example
    added html + css
    added a POST function that returns json

--- page/example.lua
+++ page/example.lua
@@ -1,11 +1,20 @@
+-- /page/example
 
-local Frame = require("red/frame")
+local Frame = require("frame")
 local frame = Frame.New("Example")
 
 
 frame:Get("/", function()
-    return "<h1>Homepage</h1>"
+    -- load and return show the example.html
+    return LoadAsset("/page/example.html")
 end)
 
 
-return frame
+frame:Post("/", function()
+    -- let a post request to the '/' path return some JSON
+    SetHeader('Content-Type', 'application/json; charset=utf-8')
+    return [[{"data": "This is an example"}]]
+end)
+
+
+return assert(frame:IsValidDefined())
